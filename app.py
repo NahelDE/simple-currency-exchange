a2627882ba6d9bf52a7088c4c553c77e3744c496
Nahel Delille <nahel.delille1507@gmail.com>
2025-11-16 17:07:30 +0100

    #2 used the api to show how much is one euro in all currency

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+all_currency_pretty_name = requests.get(
+    'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json').json()
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/all_currency_in_euro')
+def all_currency_in_euro():
+    request = requests.get("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json")
+
+    date = request.json()['date']
+    currency = request.json()['eur']
+
+    for currency_short_name, currency_pretty_name in all_currency_pretty_name.items():
+        currency[currency_short_name] = [currency_pretty_name, currency[currency_short_name]]
+
+    return render_template("all_currency_in_euro.html", date=date, currency=currency)
 
 
 if __name__ == '__main__':
